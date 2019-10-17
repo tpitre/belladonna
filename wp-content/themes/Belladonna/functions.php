b7b02e62f741a7423c4ce3f5f89f1b5bef77a5b2
Ben Voran <bvoran@gmail.com>
2019-10-17 16:41:21 -0400

    adding acf json

--- wp-content/themes/Belladonna/functions.php
+++ wp-content/themes/Belladonna/functions.php
@@ -17,6 +17,23 @@ if (!is_admin()) {
 }
 add_action('wp_enqueue_scripts', 'mytheme_enqueue_scripts');
 
+add_filter('acf/settings/save_json', 'my_acf_json_save_point');
+function my_acf_json_save_point( $path ) {
+    // update path
+    $path = get_stylesheet_directory() . '/acf-json';
+    // return
+    return $path;
+}
+
+add_filter('acf/settings/load_json', 'my_acf_json_load_point');
+function my_acf_json_load_point( $paths ) {
+    // remove original path (optional)
+    unset($paths[0]);
+    // append path
+    $paths[] = get_stylesheet_directory() . '/acf-json';
+    // return
+    return $paths;
+}
 
 /**
  *
