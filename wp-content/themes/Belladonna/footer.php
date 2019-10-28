bc0b6803edcfe4b253c580144998e5920daf9d26
Ben Voran <bvoran@gmail.com>
2019-10-28 17:54:25 -0400

    footer newsletter

--- wp-content/themes/Belladonna/footer.php
+++ wp-content/themes/Belladonna/footer.php
@@ -50,30 +50,7 @@
 
       <div class="footer_signup">
         <h6>Sign Up for Exclusive Offers!</h6>
-        <!-- MailChimp for WordPress v4.0.12 - https://wordpress.org/plugins/mailchimp-for-wp/ -->
-        <form id="mc4wp-form-1" class="mc4wp-form mc4wp-form-67" method="post" data-id="67"
-          data-name="Newsletter Signup">
-          <div class="mc4wp-form-fields">
-            <p>
-              <input type="text" name="FNAME" placeholder="First Name" required=""
-                style="background-image: url(&quot;data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAAXNSR0IArs4c6QAAAfBJREFUWAntVk1OwkAUZkoDKza4Utm61iP0AqyIDXahN2BjwiHYGU+gizap4QDuegWN7lyCbMSlCQjU7yO0TOlAi6GwgJc0fT/fzPfmzet0crmD7HsFBAvQbrcrw+Gw5fu+AfOYvgylJ4TwCoVCs1ardYTruqfj8fgV5OUMSVVT93VdP9dAzpVvm5wJHZFbg2LQ2pEYOlZ/oiDvwNcsFoseY4PBwMCrhaeCJyKWZU37KOJcYdi27QdhcuuBIb073BvTNL8ln4NeeR6NRi/wxZKQcGurQs5oNhqLshzVTMBewW/LMU3TTNlO0ieTiStjYhUIyi6DAp0xbEdgTt+LE0aCKQw24U4llsCs4ZRJrYopB6RwqnpA1YQ5NGFZ1YQ41Z5S8IQQdP5laEBRJcD4Vj5DEsW2gE6s6g3d/YP/g+BDnT7GNi2qCjTwGd6riBzHaaCEd3Js01vwCPIbmWBRx1nwAN/1ov+/drgFWIlfKpVukyYihtgkXNp4mABK+1GtVr+SBhJDbBIubVw+Cd/TDgKO2DPiN3YUo6y/nDCNEIsqTKH1en2tcwA9FKEItyDi3aIh8Gl1sRrVnSDzNFDJT1bAy5xpOYGn5fP5JuL95ZjMIn1ya7j5dPGfv0A5eAnpZUY3n5jXcoec5J67D9q+VuAPM47D3XaSeL4AAAAASUVORK5CYII=&quot;); background-repeat: no-repeat; background-attachment: scroll; background-size: 16px 18px; background-position: 98% 50%;">
-            </p>
-            <p>
-              <input type="text" name="LNAME" placeholder="Last Name">
-            </p>
-            <p>
-              <input type="email" name="EMAIL" placeholder="Email Address" required="">
-            </p>
-            <p>
-              <input type="submit" value="Sign Up">
-            </p>
-            <div style="display: none;"><input type="text" name="_mc4wp_honeypot" value="" tabindex="-1"
-                autocomplete="off"></div><input type="hidden" name="_mc4wp_timestamp" value="1571929141"><input
-              type="hidden" name="_mc4wp_form_id" value="67"><input type="hidden" name="_mc4wp_form_element_id"
-              value="mc4wp-form-1">
-          </div>
-          <div class="mc4wp-response"></div>
-        </form><!-- / MailChimp for WordPress Plugin -->
+        <?php echo do_shortcode('[mc4wp_form id="226"]'); ?>
       </div>
 
     </div>
