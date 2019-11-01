f8bf3400639f730d42ee6ff8cd666b0a92464bdf
Ben Voran <bvoran@gmail.com>
2019-11-01 16:41:16 -0400

    style updates and GTM

--- wp-content/themes/Belladonna/header.php
+++ wp-content/themes/Belladonna/header.php
@@ -47,6 +47,14 @@
 
     <meta name="p:domain_verify" content="59c1b9f9b1739a7e5142127e0118c65b"/>
 
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-MM3G6VQ');</script>
+    <!-- End Google Tag Manager -->
+
   <!--[if lt IE 9]>
 <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
 <![endif]-->
@@ -56,6 +64,11 @@
   </head>
 
   <body <?php body_class(); ?>>
+
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MM3G6VQ"
+  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
 
   <?php
   if (is_page()):
