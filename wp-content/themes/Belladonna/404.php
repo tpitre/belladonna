ef31c3253cb226ee6abc4b7fe0055db20eb21558
Terrance Pitre <tpitre@southleft.com>
2019-11-03 08:21:30 -0600

    Styling the 404.

--- wp-content/themes/Belladonna/404.php
+++ wp-content/themes/Belladonna/404.php
@@ -10,46 +10,20 @@
 get_header(); ?>
 
 <div id="main" class="clearfix">
+  <div class="main_section_wrapper">
+    <h1 class="page-title Red">This page is missing</h1>
 
- <div class="container">
+    <div class="main_content">
 
+      <h2 style="text-align:left">It seems we can&rsquo;t find what you&rsquo;re looking for.</h2>
 
-      
- <!-- Masthead
-      ================================================== -->
-      <header class="jumbotron subhead" id="overview">
-        <h1><?php _e( 'This is Embarrassing', 'bootstrapwp' ); ?></h1>
-        <p class="lead"><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching, or one of the links below, can help.', 'bootstrapwp' ); ?></p>
-      </header>
-	  
-      <div class="row content">
-		<div class="span8">					
-			<div class="well"><?php get_search_form(); ?></div><!--/.well -->
-        </div><!--/.span8 -->
-            
-        <div class="row">
-            <div class="span4">
-                <h2>All Pages</h2>
-                <?php wp_page_menu(); ?>
-            </div><!--/.span4 -->
-            
-            <div class="span4">
-                <?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
-            
-                <h2><?php _e( 'Most Used Categories', 'bootstrapwp' ); ?></h2>
-                <ul>
-                <?php wp_list_categories( array( 'orderby' => 'count', 'order' => 'DESC', 'show_count' => 1, 'title_li' => '', 'number' => 10 ) ); ?>
-                </ul>
-            </div><!--/.span4 -->
-            
-           </div><!--.row-->
-   
-        </div><!--.row.content-->
+      <p>We apologize for any inconvenience. Perhaps one of the links below, can help.</p>
 
-	</div><!-- .container -->
+      <p>
+        <h2 style="text-align:left">All Pages</h2>
+        <?php wp_page_menu(); ?>
+      </p>
 
-	</div><!-- #main -->
-                    
- <?php get_sidebar(); ?>
-
-<?php get_footer(); ?>
+    </div>
+  </div>
+</div>
